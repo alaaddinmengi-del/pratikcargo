3478237111cc381fc915016505b1c66dfb2a9c0f
alaaddinmengi-del <alaaddinmengi@gmail.com>
2025-09-09 11:35:59 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
+export const metadata = {
+  title: "Pratikcargo",
+  description: "Basit Next.js test"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
+
 
